6d9893da48b1a88d31e4d890b8c6f45ffcb1639a
José Sousa <josesousa9000@gmail.com>
2020-04-07 01:06:42 +0100

    Code splitting

--- frontend/components/App/index.js
+++ frontend/components/App/index.js
@@ -1,7 +1,16 @@
 import React from "react";
+import asyncImport from "react-imported-component";
 
 import styles from "./index.module.css";
 
+// Code splitting, as an example
+const Text = asyncImport(() => import("root/components/Text"));
+
 export default function App() {
-  return <div className={styles.root}>Hello Worlds!</div>;
+  return (
+    <div className={styles.root}>
+      <p>Hello World!</p>
+      <Text>This is an async component</Text>
+    </div>
+  );
 }
---
+++ frontend/components/Text/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Text({ children }) {
+  return <p>{children}</p>;
+}
