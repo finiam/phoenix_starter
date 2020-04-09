ea9a9cad804f0d5fe5ef04b9b93781c82a0a85f3
José Sousa <josesousa9000@gmail.com>
2020-04-09 22:29:46 +0100

    Enable gzip

--- lib/phoenix_starter_web/endpoint.ex
+++ lib/phoenix_starter_web/endpoint.ex
@@ -21,7 +21,7 @@ defmodule PhoenixStarterWeb.Endpoint do
   plug Plug.Static,
     at: "/",
     from: :phoenix_starter,
-    gzip: false,
+    gzip: true,
     only: ~w(css fonts images js favicon.ico robots.txt)
 
   # Code reloading can be explicitly enabled under the
