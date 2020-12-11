ded59181d7ab11ef9e9a16d6a5d554bb827997ae
Francisco Sousa <francisco.sousa@hey.com>
2020-12-11 16:48:09 +0000

    Add error

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,10 +1,11 @@
 /* eslint-disable global-require, import/no-extraneous-dependencies */
+const path = require("path");
 
 module.exports = {
   plugins: [
     process.env.NODE_ENV === "production" ? require("autoprefixer")() : null,
     require("postcss-custom-media")({
-      importFrom: [__dirname + "/src/styles/breakpoints.css"],
+      importFrom: [path.join(__dirname, "/src/styles/breakpoints.css")],
     }),
     require("postcss-nested"),
     process.env.NODE_ENV === "production"
--- frontend/src/components/ServerResource.tsx
+++ frontend/src/components/ServerResource.tsx
@@ -3,16 +3,23 @@ import redaxios from "redaxios";
 
 export default function ServerResource() {
   const [data, setData] = useState<undefined | any>();
+  const [error, setError] = useState<undefined | any>();
 
   async function fetchExampleData() {
-    const response = await redaxios.get(`/api/example`);
+    try {
+      const response = await redaxios.get(`/api/example`);
 
-    setData(response.data);
+      setData(response.data);
+    } catch (error) {
+      setError(error);
+    }
   }
 
   useEffect(() => {
     fetchExampleData();
   }, []);
+
+  if (error) return <p>error!</p>
 
   return data ? <p>API Result: {data.message}</p> : <p>loading</p>;
 }
