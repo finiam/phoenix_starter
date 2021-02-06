718e5da0dccd6730558d5205fdfdd84cbc68bf2c
Francisco Sousa <francisco.sousa@hey.com>
2021-02-06 18:27:47 +0000

    SSL

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -12,7 +12,7 @@ database_url =
     """
 
 config :phoenix_starter, PhoenixStarter.Repo,
-  # ssl: true,
+  ssl: true,
   url: database_url,
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
